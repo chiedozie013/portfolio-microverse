ad4eba7c1792425b86a817b5ec7d677ba90e6248
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-07-26 09:34:57 +0100

    updated file

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const btn = document.querySelector(".hamburger");
-const showHamburger = document.querySelector(".showHamburger");
-const menuLinks = document.querySelectorAll(".menu-links");
-const closeBtn = document.querySelector(".close-cross");
+const btn = document.querySelector('.hamburger');
+const showHamburger = document.querySelector('.showHamburger');
+const menuLinks = document.querySelectorAll('.menu-links');
+const closeBtn = document.querySelector('.close-cross');
 
 function openButton() {
-  showHamburger.classList.add("show-showHamburger");
+  showHamburger.classList.add('show-showHamburger');
 }
 
-btn.addEventListener("click", openButton);
+btn.addEventListener('click', openButton);
 
 function closeButton() {
-  showHamburger.classList.remove("show-showHamburger");
+  showHamburger.classList.remove('show-showHamburger');
 }
 
-closeBtn.addEventListener("click", closeButton);
+closeBtn.addEventListener('click', closeButton);
 
-menuLinks.forEach((link) =>
-  link.addEventListener("click", () => {
-    showHamburger.classList.remove("show-showHamburger");
-  })
-);
+menuLinks.forEach((link) => link.addEventListener('click', () => {
+  showHamburger.classList.remove('show-showHamburger');
+}));
