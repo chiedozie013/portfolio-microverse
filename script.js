016081f6b71615a2858cf5adfa613b229cc0adbb
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-07-27 00:19:02 +0100

    Added js to iterte the first card

--- script.js
+++ script.js
@@ -1,20 +1,180 @@
-const btn = document.querySelector('.hamburger');
-const showHamburger = document.querySelector('.showHamburger');
-const menuLinks = document.querySelectorAll('.menu-links');
-const closeBtn = document.querySelector('.close-cross');
+const btn = document.querySelector(".hamburger");
+const showHamburger = document.querySelector(".showHamburger");
+const menuLinks = document.querySelectorAll(".menu-links");
+const closeBtn = document.querySelector(".close-cross");
+// Card Function
+const allCards = document.querySelector(".works-main");
+const modalSection = document.querySelector(".modal-section");
+const firstCard = document.querySelector(".workDetails");
 
 function openButton() {
-  showHamburger.classList.add('show-showHamburger');
+  showHamburger.classList.add("show-showHamburger");
 }
 
-btn.addEventListener('click', openButton);
+btn.addEventListener("click", openButton);
 
 function closeButton() {
-  showHamburger.classList.remove('show-showHamburger');
+  showHamburger.classList.remove("show-showHamburger");
 }
 
-closeBtn.addEventListener('click', closeButton);
+closeBtn.addEventListener("click", closeButton);
 
-menuLinks.forEach((link) => link.addEventListener('click', () => {
-  showHamburger.classList.remove('show-showHamburger');
-}));
+menuLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    showHamburger.classList.remove("show-showHamburger");
+  })
+);
+
+// Pop-up window array of object
+const projectArr = [
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 1,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 2,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 3,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 4,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 5,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 6,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+  {
+    name: "Professional Art Printing Data",
+    nameCard: "Multi Post Stories",
+    number: 7,
+    Description:
+      "A daily selection of privately personalized reads; no account or sign-ups required,has been the industry's standard",
+    descriptionCard:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+    descriptionGym:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaL",
+    technology1: "html",
+    technology2: "bootstrap",
+    technology3: "ruby",
+    technology4: "Ruby on rails",
+    technology5: "css",
+    button: "see project",
+    btnCard1: "See Live",
+    btnCard2: "See Source",
+  },
+];
+
+// Function to iterate through the cards
+const cardsHTML = function () {
+  // First Card intration
+  for (let index = 0; index < projectArr.length; index++) {
+    firstCard.innerHTML = `
+    <h3>${projectArr[index].nameCard}</h3>
+    <p>${projectArr[index].descriptionGym}</p>
+    <ul class="work-lang">
+      <li>${projectArr[index].technology5}</li>
+      <li>${projectArr[index].technology1}</li>
+      <li>${projectArr[index].technology2}</li>
+      <li>${projectArr[index].technology3}</li>
+    </ul>
+    <button class="btn" href="#">${projectArr[index].button}</button>
+    `;
+  }
+
+  // To interate the remaining cards
+  for (let index = 1; index < projectArr.length; index++) {}
+};
+
+window.addEventListener("load", cardsHTML);
